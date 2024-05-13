83ecaabfbf901a6b40a922851ee5bae62a77d776
flex6699 <54765869+flex6699@users.noreply.github.com>
2024-05-13 14:56:50 +0530

    final version1.1

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
 
-import { FooterOverlay, Newsletter } from "../../components";
+import { FooterOverlay } from "../../components";
 import { images } from "../../constants";
 import "./Footer.css";
 
 const Footer = () => (
   <div className="app__footer section__padding">
     <FooterOverlay />
-    <Newsletter />
 
     <div className="app__footer-links">
       <div className="app__footer-links_contact">
@@ -44,7 +43,9 @@ const Footer = () => (
       </div>
     </div>
     <div className="footer__copyright">
-      <p className="p__opensans">2024 Sanjay Bhauriyal . All Rights Reserved.</p>
+      <p className="p__opensans">
+        2024 Sanjay Bhauriyal . All Rights Reserved.
+      </p>
     </div>
   </div>
 );
